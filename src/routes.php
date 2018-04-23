f9f8612ef8afd127062a8c4fb5c6833cc3f46cd7
Jan-Espen Oversand <jan.espen.oversand@cerpus.com>
2018-04-23 15:05:15 +0200

    Middleware group cerpusauth.

--- src/routes.php
+++ src/routes.php
@@ -1,5 +1,5 @@
 <?php
 
-Route::group(['namespace' => 'Cerpus\LaravelAuth\Controllers'], function () {
+Route::group(['namespace' => 'Cerpus\LaravelAuth\Controllers', 'middleware' => 'cerpusauth'], function () {
     Route::post('/jwt/create', 'JWTController@getJwt')->name('create.jwt');
 });
