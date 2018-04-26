04f6da0d684a24934381959d5d1523e01be59946
Jan-Espen Oversand <jan.espen.oversand@cerpus.com>
2018-04-26 09:42:20 +0200

    Oauth2 return endpoint is GET method.

--- src/routes.php
+++ src/routes.php
@@ -2,5 +2,5 @@
 
 Route::group(['namespace' => 'Cerpus\LaravelAuth\Controllers', 'middleware' => 'cerpusauth'], function () {
     Route::post('/jwt/create', 'JWTController@getJwt')->name('create.jwt');
-    Route::post('/oauth2/return', 'Oauth2ReturnController@returnEndpoint')->name('oauth2.return');
+    Route::get('/oauth2/return', 'Oauth2ReturnController@returnEndpoint')->name('oauth2.return');
 });
