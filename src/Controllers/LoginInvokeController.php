d8e0db0c50374241baba8dd1f9e2a167144c0939
Jan-Espen Oversand <jan.espen.oversand@cerpus.com>
2018-05-02 09:24:10 +0200

    Use the oauth2 return endpoint for return url.

--- src/Controllers/LoginInvokeController.php
+++ src/Controllers/LoginInvokeController.php
@@ -12,12 +12,18 @@ class LoginInvokeController extends Controller {
     public function getLoginData(Request $request) {
         $redirect = $request->get("redirect", null);
         $abort = $request->get("abort", null);
-        $state = sha1(mt_rand().mt_rand());
         /** @var $cerpusAuthService CerpusAuthService */
         $cerpusAuthService = App::make(CerpusAuthService::class);
         if (!($cerpusAuthService instanceof CerpusAuthService)) {
             throw new \Exception("Class type exception: Required CerpusAuthService");
         }
-        return response()->json($cerpusAuthService->getAuthorizeRequest()->executeApi($redirect, $abort, $state));
+        $flow = $cerpusAuthService->startFlow()
+            ->setSuccessUrl($redirect)
+            ->setFailureUrl($abort);
+        $returnUrl = route('oauth2.return');
+        $legacyAuthorizeUrl = $flow->authorizeUrl($returnUrl);
+        $legacyAuthorizeQuery = parse_url($legacyAuthorizeUrl, PHP_URL_QUERY);
+        parse_str($legacyAuthorizeQuery, $legacyAuthorizeParams);
+        return response()->json($cerpusAuthService->getAuthorizeRequest()->executeApi($returnUrl, $abort, $legacyAuthorizeParams['state']));
     }
 }
