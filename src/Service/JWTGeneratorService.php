fb5b20422a8d3363d45a0b316ba7f9c215dfce6b
Jan-Espen Oversand <jan.espen.oversand@cerpus.com>
2018-10-18 08:58:28 +0200

    EDP-107 Check for existence of privkey and contexts

--- src/Service/JWTGeneratorService.php
+++ src/Service/JWTGeneratorService.php
@@ -6,19 +6,33 @@ namespace Cerpus\LaravelAuth\Service;
 use Cerpus\JWTSupport\JWTSigner;
 
 class JWTGeneratorService {
-    public function signJwt($contextName, $payload) {
+
+    /**
+     * @param string $contextName
+     * @param array $payload
+     *
+     * @return string
+     * @throws \Exception
+     */
+    public function signJwt(string $contextName, array $payload)/*:? string*/ {
         $jwtContexts = config('cerpusauth.jwt-contexts');
         $privKeys = [];
         $config = [];
         foreach ($jwtContexts as $jwtContext) {
-            $privKeys[$jwtContext['name']] = $jwtContext['privkey'];
-            $config[$jwtContext['name']] = $jwtContext;
+            if (isset($jwtContext['privkey'])) {
+                $privKeys[$jwtContext['name']] = $jwtContext['privkey'];
+                $config[$jwtContext['name']] = $jwtContext;
+            }
         }
 
-        $privKey = $privKeys[$contextName];
+        if (isset($privKeys[$contextName])) {
+            $privKey = $privKeys[$contextName];
 
-        $signer = new JWTSigner($privKey);
+            $signer = new JWTSigner($privKey);
 
-        return $signer->sign($payload);
+            return $signer->sign($payload);
+        } else {
+            throw new \Exception("Context not found ".$contextName);
+        }
     }
 }
