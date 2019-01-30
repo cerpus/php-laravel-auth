9c8f562fe901988a744d79684c7877c3dc0bd528
Jan-Espen Oversand <jan.espen.oversand@cerpus.com>
2019-01-30 13:28:37 +0100

    CCC-3324 New js build

--- src/Config/cerpusauth.php
+++ src/Config/cerpusauth.php
@@ -34,7 +34,7 @@ return (function () {
             'cerpusauth-default-key-changeme'),
         'jwt-contexts' => $jwtContexts,
         'assetPath' => 'resources/reactive-login/static',
-        'css' => 'css/main.f6f08257.css',
-        'js' => 'js/main.c91b9bd5.js'
+        'css' => 'css/main.91dd0cd1.css',
+        'js' => 'js/main.080488dc.js'
     ];
 })();
