adb7e01da3ff55795a6d37ed3a7e78770779e387
Jan-Espen Oversand <jan.espen.oversand@cerpus.com>
2019-03-27 15:28:30 +0100

    EDP-216 New js build trying to figure out why T&C not working

--- src/Config/cerpusauth.php
+++ src/Config/cerpusauth.php
@@ -35,6 +35,6 @@ return (function () {
         'jwt-contexts' => $jwtContexts,
         'assetPath' => 'resources/reactive-login/static',
         'css' => 'css/main.a2d6fe11.css',
-        'js' => 'js/main.b77818b1.js'
+        'js' => 'js/main.e9de6683.js'
     ];
 })();
