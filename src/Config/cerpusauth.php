9cd0b334ef8ae939d00d6ead2693842b77cdcf47
Jan-Espen Oversand <jan.espen.oversand@cerpus.com>
2019-03-28 10:46:59 +0100

    EDP-216 New js build, trying to fix prefill issues when doing auto submit with T&C

--- src/Config/cerpusauth.php
+++ src/Config/cerpusauth.php
@@ -35,6 +35,6 @@ return (function () {
         'jwt-contexts' => $jwtContexts,
         'assetPath' => 'resources/reactive-login/static',
         'css' => 'css/main.a2d6fe11.css',
-        'js' => 'js/main.48f012cf.js'
+        'js' => 'js/main.8123d8f5.js'
     ];
 })();
