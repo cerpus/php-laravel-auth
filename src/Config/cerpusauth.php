d673bb511df473acdda313dc9099a3f12f4bb3b4
Jan-Espen Oversand <jan.espen.oversand@cerpus.com>
2019-03-28 10:18:34 +0100

    EDP-216 New js build, trying to fix prefill of fields when T&C is shown before auto submit

--- src/Config/cerpusauth.php
+++ src/Config/cerpusauth.php
@@ -35,6 +35,6 @@ return (function () {
         'jwt-contexts' => $jwtContexts,
         'assetPath' => 'resources/reactive-login/static',
         'css' => 'css/main.a2d6fe11.css',
-        'js' => 'js/main.72b3984a.js'
+        'js' => 'js/main.48f012cf.js'
     ];
 })();
