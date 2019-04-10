09cc0252c317afc0f63887bc14638085b9fcb3cc
Jan-Espen Oversand <jan.espen.oversand@cerpus.com>
2019-04-10 14:26:45 +0200

    CCC-3809 New js build

--- src/Config/cerpusauth.php
+++ src/Config/cerpusauth.php
@@ -35,6 +35,6 @@ return (function () {
         'jwt-contexts' => $jwtContexts,
         'assetPath' => 'resources/reactive-login/static',
         'css' => 'css/main.a2d6fe11.css',
-        'js' => 'js/main.8c7c81b0.js'
+        'js' => 'js/main.69948309.js'
     ];
 })();
