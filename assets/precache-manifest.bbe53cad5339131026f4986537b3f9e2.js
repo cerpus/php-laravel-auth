d2b3f44e130181958db28cb72d9895820fbb2a0d
Jan-Espen Oversand <jan.espen.oversand@cerpus.com>
2021-01-13 13:51:43 +0100

    EDL-845 New build of js app

---
+++ assets/precache-manifest.bbe53cad5339131026f4986537b3f9e2.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "a896680134eb0a610b7f4ce472aa7a3d",
+    "url": "/resources/reactive-login/static/media/icomoon.a8966801.ttf"
+  },
+  {
+    "revision": "102b99b0abb362344b11",
+    "url": "/resources/reactive-login/static/css/main.63675532.chunk.css"
+  },
+  {
+    "revision": "2a53b310723cde61ed22",
+    "url": "/resources/reactive-login/static/js/runtime~main.0442a969.js"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/resources/reactive-login/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "3967a105941f7f6ccfb1",
+    "url": "/resources/reactive-login/static/js/2.0bd8d412.chunk.js"
+  },
+  {
+    "revision": "cb1f3ee10b8115f8d6d2d5d2cf37424e",
+    "url": "/resources/reactive-login/static/media/emblemhvit-01.cb1f3ee1.png"
+  },
+  {
+    "revision": "f54ba96af4d2e006d951b6b3234609b9",
+    "url": "/resources/reactive-login/static/media/BB_icn_05_eye1.f54ba96a.svg"
+  },
+  {
+    "revision": "2c22bb67080ee4336ec45d51d33c060e",
+    "url": "/resources/reactive-login/static/media/BB_img_authun.2c22bb67.svg"
+  },
+  {
+    "revision": "30e6ffa22610eeca2f965863ce44adea",
+    "url": "/resources/reactive-login/static/media/icomoon.30e6ffa2.eot"
+  },
+  {
+    "revision": "102b99b0abb362344b11",
+    "url": "/resources/reactive-login/static/js/main.0c1f9fe1.chunk.js"
+  },
+  {
+    "revision": "64cea04816acf8fcafc39e5218e249eb",
+    "url": "/resources/reactive-login/static/media/icomoon.64cea048.woff"
+  },
+  {
+    "revision": "d5b4d659db97fc0dfd568da7683c755f",
+    "url": "/resources/reactive-login/static/media/icomoon.d5b4d659.svg"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/resources/reactive-login/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/resources/reactive-login/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/resources/reactive-login/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/resources/reactive-login/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "3967a105941f7f6ccfb1",
+    "url": "/resources/reactive-login/static/css/2.8304f845.chunk.css"
+  },
+  {
+    "revision": "26c55ffd25c364f7337fae36e989b2c2",
+    "url": "/resources/reactive-login/index.html"
+  }
+];
--- assets/service-worker.js
+++ assets/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/resources/reactive-login/precache-manifest.c8821a76589a634a30ed0cd40acf4157.js"
+  "/resources/reactive-login/precache-manifest.bbe53cad5339131026f4986537b3f9e2.js"
 );
 
 workbox.clientsClaim();
